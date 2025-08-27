fd20a702c40b9cebd194b6582862b39e514333fb
JonathanGrauberg <jonathangrauberg@gmail.com>
2025-08-27 10:32:58 -0300

    presentacion de inicio

--- components/ModalInicio.tsx
+++ components/ModalInicio.tsx
@@ -1,6 +1,14 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { __bgm } from './BgmProvider'
+import { DM_Serif_Display } from 'next/font/google'
+
+const dmSerif = DM_Serif_Display({
+  subsets: ['latin'],
+  weight: '400',
+})
+
+
 export default function ModalInicio() {
   const [mostrar, setMostrar] = useState(false)
   // Verificar si el modal ya fue mostrado
@@ -35,18 +43,24 @@ export default function ModalInicio() {
   }
   if (!mostrar) return null
   return (
-    <div style={{ marginTop: '480px' }} className="absolute inset-0 w-full z-[9999] px-4 pb-6 pointer-events-none">
-      <div className="bg-white text-black rounded-t-xl sm:rounded-xl px-5 py-6 shadow-lg text-center pointer-events-auto">
-        <h2 className="text-xl font-semibold mb-2">¡Bienvenid@ a nuestra invitación!</h2>
+    <div style={{ background: '#eec461f8'}} className="absolute inset-0 w-full z-[9999] px-4 pb-6 pointer-events-none">
+      <div style={{ marginTop: '30vh'}} className="text-white sm:rounded-xl px-5 py-6 text-center pointer-events-auto">
+        <h2 style={{ fontSize: '40px'}} className={`${dmSerif.className} mb-2`}>
+          ¡Bienvenidos a nuestra invitación!
+        </h2>
         <p className="text-sm text-gray-700 mb-4">
           Con mucho cariño, gracias a Silvia Tejeira por regalarnos su canción para ambientar este recorrido.
         </p>
         <button
           onClick={handleComenzar}
-          className="inline-flex items-center justify-center rounded-full bg-doradoboda text-white px-5 py-2 text-sm font-medium shadow hover:brightness-95 active:scale-[0.98]">
+          style={{background: '#f4882adf',
+          color: 'white',
+          padding: '10px 24px',
+          border: 'none',
+          borderRadius: '9999px'}}className="inline-flex items-center justify-center rounded-full bg-doradoboda text-white px-5 py-2 text-sm font-medium shadow hover:brightness-95 active:scale-[0.98]">
           Comenzar
         </button>
-        <p className="text-[11px] text-gray-500 mt-3">
+        <p style={{ fontSize: '11px'}}className="text-gray-500 mt-4">
           Podrás silenciarla cuando quieras.
         </p>
       </div>
